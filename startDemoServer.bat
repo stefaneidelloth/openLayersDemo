93935e744667dc24fafe2064c23998f62b5daed9
stefaneidelloth <matameko@posteo.de>
2019-12-05 11:52:20 +0100

    initial commit

---
+++ startDemoServer.bat
@@ -0,0 +1,4 @@
+REM start cmd /c explorer d:\treezjs
+
+start cmd /c C:\EclipsePython\App\jdk\bin\java.exe -jar treezServer.jar
+start "" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://localhost:8080/demo.html
